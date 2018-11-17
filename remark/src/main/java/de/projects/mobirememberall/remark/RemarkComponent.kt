ad2bdda3298a8a8577233cca3642a99ef8d1442a
Andrei Precup <andreiprecup69@gmail.com>
2018-11-18 00:20:59 +0200

    Finish UI.

--- remark/src/main/java/de/projects/mobirememberall/remark/RemarkComponent.kt
+++ remark/src/main/java/de/projects/mobirememberall/remark/RemarkComponent.kt
@@ -1,13 +1,38 @@
 package de.projects.mobirememberall.remark
 
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.verticalLayout
+import android.view.Gravity
+import android.widget.LinearLayout
+import org.jetbrains.anko.*
 
 class RemarkComponent : AnkoComponent<RemarkActivity> {
 	override fun createView(ui: AnkoContext<RemarkActivity>) = with(ui) {
 		verticalLayout {
-
+			backgroundColor = context.getColor(R.color.gray)
+			editText {
+				textColor = context.getColor(R.color.white)
+				lines = 5
+			}
+			linearLayout {
+				topPadding = dip(5)
+				orientation = LinearLayout.VERTICAL
+				gravity = Gravity.CENTER
+				button(context.getString(R.string.submit)) {
+					textColor = context.getColor(R.color.white)
+					backgroundColor = context.getColor(R.color.green)
+				}.lparams(width = wrapContent) {
+					height = dip(40)
+					topMargin = dip(10)
+					horizontalMargin = dip(20)
+				}
+				button(context.getString(R.string.submit)) {
+					textColor = context.getColor(R.color.white)
+					backgroundColor = context.getColor(R.color.red)
+				}.lparams(width = wrapContent) {
+					height = dip(40)
+					topMargin = dip(10)
+					horizontalMargin = dip(20)
+				}
+			}
 		}
 	}
 }
